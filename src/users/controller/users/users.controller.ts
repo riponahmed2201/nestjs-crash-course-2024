94dea4977cdd202b02db8343fbcf6e88ec79011c
ripon <riponahmed2201@gmail.com>
2024-03-07 16:30:20 +0600

    validation added

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
 
     @Get()
-    getUsers() {
+    getUsers(@Query('sortBy') sortBy: string) {
+        console.log(sortBy);
+
         return [{ username: 'Ripon', email: 'ripon@gmail.com' }];
     }
 
     @Post('create')
+    @UsePipes(new ValidationPipe())
     createUser(@Body() userData: CreateUserDto) {
+        console.log(userData);
+        return {};
+    }
 
+    @Get(':id')
+    getUserById(@Param('id') id: string) {
+        console.log(id);
+        return { id };
     }
 }
