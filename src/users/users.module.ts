60246c00d1c562ad38d1f7f2e06b3dd936d5d55c
ripon <riponahmed2201@gmail.com>
2024-03-08 16:07:37 +0600

    nestjs middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './controller/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { ExampleMiddleware } from './middlewares/example/example.middleware';
+import { AnotherMiddleware } from './middlewares/another/another.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(ExampleMiddleware).forRoutes('users');
+    // consumer.apply(ExampleMiddleware).forRoutes(UsersController);
+    consumer.apply(ExampleMiddleware).forRoutes(
+      {
+        path: 'users',
+        method: RequestMethod.GET
+      },
+      {
+        path: 'users/:id',
+        method: RequestMethod.GET
+      }
+    ).apply(AnotherMiddleware).forRoutes(
+      {
+        path: 'users',
+        method: RequestMethod.GET
+      },
+      {
+        path: 'users/:id',
+        method: RequestMethod.GET
+      });
+  }
+}
